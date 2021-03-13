7963a5b245fec8b5929dea22ab3c9a78a36f0db4
jlew24asu <jlew24asu@gmail.com>
2021-03-13 15:55:11 -0600

    stats

---
+++ stats.py
@@ -0,0 +1,16 @@
+#Python stuff by Jeff
+
+values = [43, 56, 67, 89, 45]
+weights = [4, 8, 2, 2, 4]
+
+weights_shares = []
+for i in range(0, len(weights)):
+    weights_shares.append(weights[i] / sum(weights))
+
+print(weights_shares)
+
+weightedValues = []
+for i in range(0, len(values)):
+    weightedValues.append(values[i] * weights_shares[i])
+
+print(weightedValues)
